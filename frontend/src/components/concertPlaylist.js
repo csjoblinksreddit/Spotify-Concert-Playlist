13994b32a79293b8813ea062c82bbacb2bbbcd40
Miguel Villanueva <97miguel15@gmail.com>
2020-07-27 08:54:03 -0400

    fixed multiple playlist bug and added merge functionality

--- frontend/src/components/concertPlaylist.js
+++ frontend/src/components/concertPlaylist.js
@@ -3,12 +3,17 @@ import logo from '../logo.svg';
 import '../App.css';
 import { Input } from 'antd';
 import 'antd/dist/antd.css';
+import {Button, Form} from 'reactstrap'
+
 import '../styles/playlist.css'
 const { Search } = Input;
 
+//TO BE REPLACED WITH PROPS
+var concertArtists = ['Frank Ocean', 'Kendrick Lamar', 'Dr. Dre'];
+
 let Spotify = require('spotify-web-api-js');
 let spotifyApi = new Spotify();
-spotifyApi.setAccessToken('BQD6ETs3w3Bep1VGnyaeEwB9U4smnFPKFrzghyJeZcKeEqCSaDT89ujbmLnzK4ET-m7X3cdlm8qiEP4rz8VrOnfF6B8z_bHpfr6WWs7T9GqPzrSKcb7BV_6fr5UWp_vBCu83ONZJQgHiMKmY3CtqDhCfml66IJdm8Toqc6v-27D4ZE4V1KjdvVWmLIRl3B40UsW5&refresh_token=AQC8LFBV3Aa3IrqtVrHY7F7nsFvkTxOSJnu9iIBVFpI1m5FIHFEsM8262A-KlCcuChI2swHvW4b-aSzVXVvW0-Pf2fNerNS0csGwoiUHz9uV0p4P3DquwgiFdk-iqJKP3do');
+spotifyApi.setAccessToken('BQDb99h_9FpoNOPFOQoDWdnTrrBnF5lon8pyNcm-Zgf3H8aKg385mAU3QeQHqFy9KDh3ghoHX1kMOmP4V2D5-Mu9_mw-gaEYYG1oouGBkdAiL73C4BTuwtZZTLQ8sNLU3HiPK6zd7pAvM3QHVfQQY5ecTc63Y1w0_VBLfKhDrIFxiB7W-rltLFDDu3z8uVKaZIgY&refresh_token=AQC-CFb2J08uWzyiLPJtNo894EIVojCI8771RuHQp6L93ABak86EKaGoR5TLd9I6A7UeMDw6qTNdhmmopxEyqgvFdNYrNMkTdrvim7j784v_gojuB0J-ltvi87IJCfL5z6Y');
 
 class Playlist extends Component {
   constructor(props) {
@@ -23,6 +28,12 @@ class Playlist extends Component {
     }
   }
 
+  //ISSUE: MAKES A NEW PLAYLIST EVERY TIME PAGE IS REFRESHED
+  componentDidMount(){
+    this.getUser();
+    concertArtists.forEach(artist => this.searchArtist(artist));
+  }
+
   sliceUserId = (url) => {
       let string = url.split(/[/?]/);
       return string[5]
@@ -34,9 +45,8 @@ class Playlist extends Component {
             this.setState({
                 userId: this.sliceUserId(data.href)
             })
-            console.log(this.state.userId)
             this.createPlaylist();
-
+            
         },
         (err) => {
         console.error(err);
@@ -45,6 +55,7 @@ class Playlist extends Component {
   }
 
   searchArtist = (artist) => {
+    console.log(artist);
     spotifyApi.searchArtists(artist).then(
         (data) => {
             if(data !== undefined) {
@@ -75,7 +86,6 @@ class Playlist extends Component {
                     topTracksIds:this.state.topTracksIds.concat(track.uri) //storing track id's in an array
                 })
             })
-            this.getUser();
           },
           (err) => {
               console.log(err)
@@ -88,8 +98,7 @@ class Playlist extends Component {
           (data) => {
               this.setState({
                   userPlaylistId: data.id
-              })
-              this.addTracksToPlaylist();
+              })              
           },
           (err) => {
               console.log(err)
@@ -120,6 +129,8 @@ class Playlist extends Component {
             <li>{this.state.artist}</li>
             <li>{this.state.artistId}</li>
           </ul>
+          {/*EDIT TO STOP MAKING REPEATED ARTIST ENTRIES*/}
+          <Button placeholder="finalize playlist" onClick={this.addTracksToPlaylist}>Finalize!</Button>
         </header>
       </div>
     );
