7a515ae85bdb9be418c01c891be75ed7e5690a89
csjoblinksreddit <67166406+csjoblinksreddit@users.noreply.github.com>
2020-08-03 13:18:11 -0500

    [25] Adding spotify login server (#6)
    * adding the spotify backend
    
    * Added spotify login feature
    
    * Update index.js

---
+++ frontend/src/components/home.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import SpotifyWebApi from 'spotify-web-api-js';
+import '../App.css';
+const spotifyApi = new SpotifyWebApi();
+
+class Home extends Component {
+
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+  }
+  componentDidMount() {
+
+    if (spotifyApi.getAccessToken()) {
+      /*TODO:
+       This method needs to be changed to create a playlist. At the moment 
+       it gets what currently playing and sets it to the state object
+      */
+      spotifyApi.getMyCurrentPlaybackState()
+        .then((response) => {
+          console.log(response.item)
+          if(response){
+            this.setState({
+              nowPlaying: {
+                name: response.item.name,
+                albumArt: response.item.album.images[0].url
+              }
+            });
+          }
+          
+        })
+    }
+
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q)
+    }
+    return hashParams;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+            HOME PAGE
+          </h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Home;
