0fd14956aa47844ede367f24ea0d64b85bebeae6
Hüseyin Bera Bulut <berabulut@gmail.com>
2020-08-07 19:44:49 +0300

    added latest changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './components/navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          INDEX PAGE 
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Navbar from './components/navbar'
-import NavBar from './components/newNavbar'
+import App from './App'
 
 
 ReactDOM.render(
   <React.StrictMode>
-      <NavBar />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
