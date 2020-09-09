ae8d43cf6223afe3f6967f0bc3799ea11e2694a1
Hüseyin Bera Bulut <berabulut@gmail.com>
2020-09-09 23:21:56 +0300

    fix

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -5,7 +5,7 @@ import './footer.scss';
 const Footer = () => {
 	return(
 		<Row id="footer-row">
-			<Col style={{width: '50%'}}>Made by Miguel Zavala, Miguel VillAnueva and Hüseyin Bera Bulut</Col>
+			<Col style={{width: '50%'}}>Made by Miguel Zavala, Miguel Villanueva and Hüseyin Bera Bulut</Col>
 			<Col>@2020</Col>
 		</Row>
 	)
